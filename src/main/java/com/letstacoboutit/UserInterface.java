8a98d637f2f70be9a6b6c235e257eab864952560
Tannaah <stagy28@gmail.com>
2025-11-12 12:54:35 -0500

    Added createNewOrder Method.

--- src/main/java/com/letstacoboutit/UserInterface.java
+++ src/main/java/com/letstacoboutit/UserInterface.java
@@ -32,6 +32,42 @@ public class UserInterface {
         console.printMessage("2. View current order");
         console.printMessage("3. Exit");
     }
+
+    private void createNewOrder() {
+        String name = console.readString("Enter customer name");
+        currentOrder = new Order(name);
+
+        boolean addingTacos = true;
+        while (addingTacos) {
+            console.printHeader("Create a Taco");
+
+            String size = console.readString("Enter size (Single Taco, 3-Taco Plate, Burrito)");
+            String shell = console.readString("Enter shell type (corn, flour, hard shell, bowl)");
+
+            Taco taco = new Taco(size, shell);
+
+            // toppings
+            String meat = console.readString("Add a meat (or leave blank to skip)");
+            if (!meat.isBlank()) taco.addMeat(meat);
+
+            String cheese = console.readString("Add a cheese (or leave blank to skip)");
+            if (!cheese.isBlank()) taco.addCheese(cheese);
+
+            String topping = console.readString("Add a topping (or leave blank to skip)");
+            if (!topping.isBlank()) taco.addTopping(topping);
+
+            String sauce = console.readString("Add a sauce (or leave blank to skip)");
+            if (!sauce.isBlank()) taco.addSauce(sauce);
+
+            boolean fry = console.readYesNo("Would you like it deep fried?");
+            taco.setDeepFried(fry);
+
+            currentOrder.addTaco(taco);
+            console.printMessage("Taco added to your order!");
+
+            addingTacos = console.readYesNo("Would you like to add another taco?");
+        }
+    }
 }
 
     /* UserInterface: The Control Center!
