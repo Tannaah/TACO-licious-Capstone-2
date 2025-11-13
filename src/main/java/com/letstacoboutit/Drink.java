9347ce3fbbc7cf68ab6f7903e18ab318c587e3ca
Tannaah <stagy28@gmail.com>
2025-11-12 22:12:08 -0500

    Implemented code to my Drink Class!

--- src/main/java/com/letstacoboutit/Drink.java
+++ src/main/java/com/letstacoboutit/Drink.java
@@ -1,4 +1,20 @@
 package com.letstacoboutit;
 
-public class Drink {
+public class Drink extends MenuItem {
+
+    private String size; // small, medium, large
+
+    public Drink(String name, String size, double price) {
+        super(name, price);
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%s) - $%.2f", getName(), size, getPrice());
+    }
 }
