60dc28d3ad1a05ae1812d3a37d2b03d849c8c3cb
Tannaah <stagy28@gmail.com>
2025-11-12 22:39:11 -0500

    Implemented code to my ChipsAndSalsa Class!

--- src/main/java/com/letstacoboutit/ChipsAndSalsa.java
+++ src/main/java/com/letstacoboutit/ChipsAndSalsa.java
@@ -1,4 +1,20 @@
 package com.letstacoboutit;
 
-public class ChipsAndSalsa {
+public class ChipsAndSalsa extends MenuItem {
+
+    private String salsaType;  // mild, spicy, verde, roja, etc.
+
+    public ChipsAndSalsa(String salsaType, double price) {
+        super("Chips & Salsa", price);
+        this.salsaType = salsaType;
+    }
+
+    public String getSalsaType() {
+        return salsaType;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chips & Salsa (%s) - $%.2f", salsaType, getPrice());
+    }
 }
