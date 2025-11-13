a8812c4b30b263778105cf7a86c966dcda3e4452
Tannaah <stagy28@gmail.com>
2025-11-13 15:29:19 -0500

    Added an outline/framework to my two signature Classes, the StreetTaco and SuperBurrito.

--- src/main/java/com/letstacoboutit/models/signatures/StreetTaco.java
+++ src/main/java/com/letstacoboutit/models/signatures/StreetTaco.java
@@ -2,3 +2,19 @@ package com.letstacoboutit.models.signatures;
 
 public class StreetTaco {
 }
+
+// StreetTaco Class
+// -----------------------------
+// • A Signature Taco template.
+// • Extends the base Taco class.
+// • Automatically comes with:
+//   - 3-Taco Plate size
+//   - Corn tortillas
+//   - Not Deep-Fried
+//   - Preloaded toppings:
+//      * carne asada  (MEAT)
+//      * onions       (VEGGIE)
+//      * cilantro     (VEGGIE)
+//      * salsa verde  (SAUCE)
+//      * lime wedges  (OTHER)
+// • After selecting this item, the customer can optionally customize it by adding or removing toppings.
--- src/main/java/com/letstacoboutit/models/signatures/SuperBurrito.java
+++ src/main/java/com/letstacoboutit/models/signatures/SuperBurrito.java
@@ -2,3 +2,20 @@ package com.letstacoboutit.models.signatures;
 
 public class SuperBurrito {
 }
+
+// SuperBurrito Class
+// -----------------------------
+// • A Signature Burrito template.
+// • Extends the base Taco class.
+// • Automatically comes with:
+//   - Burrito size
+//   - Flour tortilla
+//   - Not Deep-Fried
+//   - Preloaded toppings:
+//    * carnitas        (MEAT)
+//    * cheddar         (CHEESE)
+//    * pico de gallo   (VEGGIE)
+//    * lettuce         (VEGGIE)
+//    * tomatoes        (VEGGIE)
+//    * birria dipped   (SAUCE)
+// • After selecting this item, the customer can optionally customize it further using the normal topping flow.
